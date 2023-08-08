e5c5d2027d72a75464c0b2fe31b7f55168f2575f
Sherrane Watson <Sherranewatson98@gmail.com>
2023-08-07 22:57:59 -0400

    Additions

--- Memory Card Game/ContentView.swift
+++ Memory Card Game/ContentView.swift
@@ -17,56 +17,56 @@ struct ContentView: View {
                 ForEach(emojis[0..<emojiCount], id: \.self) { emoji in CardView(content: emoji)
                 }
             }
+            Spacer()
             HStack {
-                Button(action: {
-                    emojiCount -= 1
-                }, label: {
-                    VStack {
-                        Text("Remove")
-                        Text("Card")
-                    }
-                })
+                remove
                 Spacer()
-                Button(action: {
-                    emojiCount += 1
-                }, label: {
-                    VStack {
-                        Text("Add")
-                        Text("Card")
-                    }
-                })
-                .padding(.horizontal)
+                add
             }
-        }
-        
-        .padding(.horizontal)
-        .foregroundColor(.red)
-        
-    }
-        
-        struct CardView: View {
-            var content: String
-            @State var isFaceUp: Bool = true
-            
-            var body: some View {
-                let shape = RoundedRectangle(cornerRadius: 20)
-                
-                ZStack {
-                    if isFaceUp {
-                        
-                        shape.fill().foregroundColor(.white)
-                        shape.stroke(lineWidth: 3)
-                        Text(content).font(.largeTitle)
-                    } else {
-                        shape.fill()
-                    }
-                }
-                .onTapGesture {
-                    isFaceUp.toggle()
-                }
-            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
     }
+            var remove: some View {
+            Button {
+                if emojiCount > 1 {
+                emojiCount -= 1
+                }
+            } label: {
+                Image(systemName: "minus.circle")
+                }
+            }
+                var add: some View {
+                    Button {
+                        if emojiCount < emojis.count {
+                            emojiCount += 1
+        }
+    } label: {
+    Image(systemName: "plus.circle")
+        }
+    }
+}
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+    
 
     
     
